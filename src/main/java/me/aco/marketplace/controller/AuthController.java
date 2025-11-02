a0475fdb466c48d9a0d6bb140e0bd79595993532
Aleksandar Petović <aleksandarpetrovic@live.ac.me>
2025-11-02 23:25:40 +0100

    wip

--- src/main/java/me/aco/marketplace/controller/AuthController.java
+++ src/main/java/me/aco/marketplace/controller/AuthController.java
@@ -1,14 +1,16 @@
 package me.aco.marketplace.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.aco.marketplace.dto.LoginReq;
 import me.aco.marketplace.dto.TokenResp;
+import me.aco.marketplace.dto.UserRegReq;
 import me.aco.marketplace.model.User;
 import me.aco.marketplace.service.AuthService;
 import me.aco.marketplace.service.UserService;
+import me.aco.marketplace.util.JWTUtil;
 
 @RestController
 public class AuthController {
@@ -18,7 +20,7 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public TokenResp login(LoginReq req) {
         User loadedUser = userService.getByUsername(req.getUsername());
 		if (loadedUser == null)
@@ -27,6 +29,19 @@ public class AuthController {
 			return null;
 		return new TokenResp(JWTUtil.createToken(loadedUser), authService.createAndSaveRefreshToken(req));
     }
+
+    @PostMapping("/register")
+    public String register(UserRegReq req) {
+		if (userService.getByUsername(req.getUsername()) != null)
+			return "Username already taken!";
+		try {
+			userService.saveUser(req.toUser());
+			return "User created!";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Ok";
+		}
+	}
     
 
 }
--- src/main/java/me/aco/marketplace/service/UserService.java
+++ src/main/java/me/aco/marketplace/service/UserService.java
@@ -1,10 +1,36 @@
 package me.aco.marketplace.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+import me.aco.marketplace.model.User;
 
 @Service
 public class UserService {
 
-    
+    @PersistenceUnit(unitName = "marketplace")
+	private EntityManagerFactory emf;
+
+    private EntityManager em;
+
+    public UserService() {
+        this.em = emf.createEntityManager();
+    }
+
+   public User getByUsername(String username) {
+		List<User> users = em.createNamedQuery(User.getByUsername, User.class).setParameter("username", username).getResultList();
+		if (users.size() > 0)
+			return users.get(0);
+		else
+			return null;
+	}
+	
+	public User saveUser(User user) {
+		return em.merge(user);
+	} 
     
 }
