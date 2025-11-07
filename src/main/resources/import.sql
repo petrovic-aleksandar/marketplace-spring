9025016b79f3da43fd9a3f22dff8f6e000bda4a9
Aleksandar Petović <aleksandarpetrovic@live.ac.me>
2025-11-07 01:36:19 +0100

    import fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,16 +3,16 @@ insert into appuser (id, username, password, salt, name, email, phone, balance, 
 insert into appuser (id, username, password, salt, name, email, phone, balance, role, active) values (3, 'rich', 'f2a20305e37a64a569b709b656040dbc09884b8e2f89c36480d5cb9748033c087741c5bf89a0d16900ce27f9d43ba6f180f05017d2ed38064a3bbbb252baafa6', '[B@64838816', 'Guy Rich', 'jj@gmail.com', '67 111 222', 100000.0, 'User', true);
 select setval('user_seq', 3);
 
-insert into itemtype (id, name, description, imagePath) values (1, 'Cars&Vehicles', 'Cars and other transportation. Scooters, bikes, trains, boats, yachts, airships..', 'Categories/1.jpg');
-insert into itemtype (id, name, description, imagePath) values (2, 'Home&Kitchen', 'Everything for your home, small home and kitchen electronics.', 'Categories/2.jpg');
-insert into itemtype (id, name, description, imagePath) values (3, 'Electronics', 'All kinds of devices. Gaming equipment, laptops, home appliances etc.', 'Categories/3.jpg');
-insert into itemtype (id, name, description, imagePath) values (4, 'Sports&Outdoors', 'Sports clothing and sport requisits.', 'Categories/4.jpg');
-insert into itemtype (id, name, description, imagePath) values (5, 'Womens clothing', 'Everything for women.', 'Categories/5.jpg');
-insert into itemtype (id, name, description, imagePath) values (6, 'Jewelry&Accesories', 'All kinds of jewelry and decorative items.', 'Categories/6.jpg');
-insert into itemtype (id, name, description, imagePath) values (7, 'Mens clothing', 'Clothing items for men', 'Categories/7.jpg');
-insert into itemtype (id, name, description, imagePath) values (8, 'Books&Media', 'Books and stuff.', 'Categories/8.jpg');
-insert into itemtype (id, name, description, imagePath) values (9, 'Services', 'For various intelectual or physical services.', 'Categories/9.jpg');
-insert into itemtype (id, name, description, imagePath) values (10, 'Other', 'Whatever else.', 'Categories/10.jpg');
+insert into item_type (id, name, description, imagePath) values (1, 'Cars&Vehicles', 'Cars and other transportation. Scooters, bikes, trains, boats, yachts, airships..', 'Categories/1.jpg');
+insert into item_type (id, name, description, imagePath) values (2, 'Home&Kitchen', 'Everything for your home, small home and kitchen electronics.', 'Categories/2.jpg');
+insert into item_type (id, name, description, imagePath) values (3, 'Electronics', 'All kinds of devices. Gaming equipment, laptops, home appliances etc.', 'Categories/3.jpg');
+insert into item_type (id, name, description, imagePath) values (4, 'Sports&Outdoors', 'Sports clothing and sport requisits.', 'Categories/4.jpg');
+insert into item_type (id, name, description, imagePath) values (5, 'Womens clothing', 'Everything for women.', 'Categories/5.jpg');
+insert into item_type (id, name, description, imagePath) values (6, 'Jewelry&Accesories', 'All kinds of jewelry and decorative items.', 'Categories/6.jpg');
+insert into item_type (id, name, description, imagePath) values (7, 'Mens clothing', 'Clothing items for men', 'Categories/7.jpg');
+insert into item_type (id, name, description, imagePath) values (8, 'Books&Media', 'Books and stuff.', 'Categories/8.jpg');
+insert into item_type (id, name, description, imagePath) values (9, 'Services', 'For various intelectual or physical services.', 'Categories/9.jpg');
+insert into item_type (id, name, description, imagePath) values (10, 'Other', 'Whatever else.', 'Categories/10.jpg');
 select setval('item_type_seq', 10);
 
 insert into item (id, name, description, price, type_id, active, deleted, createdAt, seller_id) values (1, 'Audi A4', 'Red color, 2021., 35 TFSI', 30000, 1, true, false, '2025-10-19 17:43:38.8328', 2);
